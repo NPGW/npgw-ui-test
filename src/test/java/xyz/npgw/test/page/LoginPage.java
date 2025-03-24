f04efb13178b374055287d9fc6d829aa18fd8321
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-03-24 12:45:23 +0300

    [!] move locator initialization outside constructor

--- src/test/java/xyz/npgw/test/page/LoginPage.java
+++ src/test/java/xyz/npgw/test/page/LoginPage.java
@@ -7,17 +7,13 @@ import xyz.npgw.test.page.base.BasePage;
 
 public final class LoginPage extends BasePage {
 
-    private final Locator emailField;
-    private final Locator passwordField;
-    private final Locator loginButton;
-    private final Locator rememberMeCheckbox;
+    private final Locator emailField = placeholder("Enter your email");
+    private final Locator passwordField = placeholder("Enter your password");
+    private final Locator loginButton = button("Login");
+    private final Locator rememberMeCheckbox = checkbox("Remember me");
 
     public LoginPage(Page page) {
         super(page);
-        emailField = placeholder("Enter your email");
-        passwordField = placeholder("Enter your password");
-        loginButton = button("Login");
-        rememberMeCheckbox = checkbox("Remember me");
     }
 
     @Step("Ввести в поле Email - email пользователя")
