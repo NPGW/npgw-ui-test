89334992cea0dc11b0277bbd8e920a8cd7438dea
Laysan Nova <laysansh@gmail.com>
2025-03-25 19:39:08 -0600

    Fix Checkstyle

--- src/test/java/xyz/npgw/test/page/base/AuthenticatedPage.java
+++ src/test/java/xyz/npgw/test/page/base/AuthenticatedPage.java
@@ -3,13 +3,14 @@ package xyz.npgw.test.page.base;
 import com.microsoft.playwright.Page;
 import xyz.npgw.test.page.Header;
 
-public class AuthenticatedPage extends BasePage{
+public class AuthenticatedPage extends BasePage {
 
     private final Header header;
     public AuthenticatedPage(Page page) {
         super(page);
         this.header = new Header(page);
     }
+
     public Header getHeader() {
         return header;
     }
