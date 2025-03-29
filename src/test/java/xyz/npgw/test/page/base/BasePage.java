3f8d11c62eb4b8bfd8e4b021656e7ace22931e58
Laysan Nova <laysansh@gmail.com>
2025-03-29 13:48:37 -0600

    ATC 11.000.02

--- src/test/java/xyz/npgw/test/page/base/BasePage.java
+++ src/test/java/xyz/npgw/test/page/base/BasePage.java
@@ -59,4 +59,8 @@ public abstract class BasePage {
     protected Locator labelExact(String text) {
         return getPage().getByLabel(text, new Page.GetByLabelOptions().setExact(true));
     }
+
+    protected Locator textExact(String text) {
+        return getPage().getByText(text, new Page.GetByTextOptions().setExact(true));
+    }
 }
