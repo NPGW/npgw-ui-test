d360275355552b52fdac0f1aa0d6325c5804a2b0
PavelSZh <psilenkov@gmail.com>
2025-03-29 17:28:47 +0300

    Refactor

--- src/test/java/xyz/npgw/test/page/SystemAdministrationPage.java
+++ src/test/java/xyz/npgw/test/page/SystemAdministrationPage.java
@@ -1,11 +1,117 @@
 package xyz.npgw.test.page;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import xyz.npgw.test.page.base.BasePageWithHeader;
+
+import java.util.List;
 
 public class SystemAdministrationPage extends BasePageWithHeader {
 
     public SystemAdministrationPage(Page page) {
         super(page);
+    }
+
+    //private final Locator
+    private final Locator merchants = placeholder("Companies and business units");
+    private final Locator addBusinessUnitButton = locator("button:has(svg[data-icon='circle-plus'])")
+            .nth(1);
+    private final Locator resetFilter = locator("button:has(svg[data-icon='xmark'])");
+    private final Locator refreshData = locator("button:has(svg[data-icon='arrows-rotate'])");
+    private final Locator selectCompanyField = placeholder("Select company");
+    private final Locator selectStatusField = placeholder("Status");
+    private final Locator merchantName = placeholder("Merchant name");
+    private final Locator checkBoxUsd = checkbox("USD");
+    private final Locator checkBoxEur = checkbox("EUR");
+    private final Locator createMerchantButton = button("Create");
+    private final Locator closeMerchantCreationWindowButton = button("Close");
+    private final Locator businessUnitName = placeholder("Business unit name");
+
+    public SystemAdministrationPage goToMerchantsTab() {
+        merchants.click();
+        return this;
+    }
+
+    public Locator getBusinessUnitName() {
+        return businessUnitName;
+    }
+
+    public SystemAdministrationPage resetFilters() {
+        resetFilter.click();
+        return this;
+    }
+
+    public SystemAdministrationPage refreshData() {
+        refreshData.click();
+        return this;
+    }
+
+    public SystemAdministrationPage selectFiltersByCompany(String companyName) {
+        selectCompanyField.click();
+        getPage().locator("text=" + companyName).click();
+        return this;
+    }
+
+    public SystemAdministrationPage selectCompanyStatus(String status) {
+        List<String> validStatuses = List.of("All", "Active", "Inactive");
+
+        if (!validStatuses.contains(status)) {
+            throw new IllegalArgumentException("Invalid status: "
+                    + status
+                    + ". Allowed values: "
+                    + validStatuses);
+        }
+
+        selectStatusField.click();
+        getPage().locator("text=" + status).click();
+        return this;
+    }
+
+    public SystemAdministrationPage setMerchantName(String name) {
+        merchantName.fill(name);
+        return this;
+    }
+
+    public SystemAdministrationPage checkUsd() {
+        checkBoxUsd.click();
+        return this;
+    }
+
+    public SystemAdministrationPage checkEur() {
+        checkBoxEur.click();
+        return this;
+    }
+
+    public SystemAdministrationPage chooseMerchantStatus(String status) {
+        List<String> validStatuses = List.of("Active", "Inactive");
+
+        if (!validStatuses.contains(status)) {
+            throw new IllegalArgumentException("Invalid status: "
+                    + status
+                    + ". Allowed values: "
+                    + validStatuses);
+        }
+
+        getPage().getByPlaceholder(status).click();
+        return this;
+    }
+
+    public SystemAdministrationPage clickOnCreateButton() {
+        createMerchantButton.click();
+        return this;
+    }
+
+    public SystemAdministrationPage clickOnCloseButton() {
+        closeMerchantCreationWindowButton.click();
+        return this;
+    }
+
+    public SystemAdministrationPage startAddingMerchant() {
+        addBusinessUnitButton.click();
+        return this;
+    }
+
+    public String getPrefilledCompanyName() {
+        return getPage().locator("input[aria-label='Company name']").getAttribute("value");
     }
 }
