cee5cf2b13308507325d569cffab94f56e0b084d
AllaBird <allavorobieva@yahoo.com>
2025-03-30 03:48:17 -0400

    Rename locator

--- src/test/java/xyz/npgw/test/page/AddCompanyWindow.java
+++ src/test/java/xyz/npgw/test/page/AddCompanyWindow.java
@@ -6,13 +6,13 @@ import xyz.npgw.test.page.base.BasePage;
 
 public class AddCompanyWindow extends BasePage {
 
-    private final Locator headerAddCompany = getPage().locator("text=Add company");
+    private final Locator addCompanyHeader = getPage().locator("text=Add company");
 
     public AddCompanyWindow(Page page) {
         super(page);
     }
 
     public Locator getHeader() {
-        return headerAddCompany;
+        return addCompanyHeader;
     }
 }
