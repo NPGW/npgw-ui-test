93a855453321fffeeef902fefb3ccbdc93cd3c17
Laysan Nova <laysansh@gmail.com>
2025-03-30 19:22:51 -0600

    removed import/clean code

--- src/test/java/xyz/npgw/test/page/SaAcquirersTab.java
+++ src/test/java/xyz/npgw/test/page/SaAcquirersTab.java
@@ -2,7 +2,6 @@ package xyz.npgw.test.page;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.Step;
 import xyz.npgw.test.page.base.BasePageWithHeader;
 
