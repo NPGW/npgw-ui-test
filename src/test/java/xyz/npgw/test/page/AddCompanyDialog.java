e7929b101289e6f4f14555db837b8a63785ec78f
AllaBird <allavorobieva@yahoo.com>
2025-04-02 21:37:44 -0400

    Add  test

--- src/test/java/xyz/npgw/test/page/AddCompanyDialog.java
+++ src/test/java/xyz/npgw/test/page/AddCompanyDialog.java
@@ -6,7 +6,21 @@ import xyz.npgw.test.page.base.BasePage;
 
 public class AddCompanyDialog extends BasePage {
 
-    private final Locator addCompanyDialogHeader = getPage().locator("section header");
+    private final Locator addCompanyDialogHeader = locator("section header");
+    private final Locator fieldPlaceholderList = locator("[data-slot='input']:not([placeholder='Search...'])");
+//    private final Locator companyNameField = locator("input[aria-label='Company name']");
+//    private final Locator companyTypeField = locator("input[aria-label='Company type']");
+//    private final Locator descriptionField = locator("input[aria-label='Description']");
+//    private final Locator websiteField = locator("input[aria-label='Website']");
+//    private final Locator primaryContactField = locator("input[aria-label='Primary contact']");
+//    private final Locator companyEmailField = locator("input[aria-label='Company email']");
+//    private final Locator countryField = locator("input[aria-label='Country']");
+//    private final Locator stateField = locator("input[aria-label='State']");
+//    private final Locator zipField = locator("input[aria-label='ZIP']");
+//    private final Locator cityField = locator("input[aria-label='City']");
+//    private final Locator phoneField = locator("input[aria-label='Phone']");
+//    private final Locator mobileField = locator("input[aria-label='Mobile']");
+//    private final Locator faxField = locator("input[aria-label='Fax']");
 
     public AddCompanyDialog(Page page) {
         super(page);
@@ -15,4 +29,63 @@ public class AddCompanyDialog extends BasePage {
     public Locator getAddCompanyDialogHeader() {
         return addCompanyDialogHeader;
     }
+
+    public Locator getFieldPlaceholderList() {
+        getPage().waitForTimeout(2000);
+        return fieldPlaceholderList;
+    }
+
+
+
+//    public Locator getCompanyNameField() {
+//        return companyNameField;
+//    }
+//
+//    public Locator getCompanyTypeField() {
+//        return companyTypeField;
+//    }
+//
+//    public Locator getDescriptionField() {
+//        return descriptionField;
+//    }
+//
+//    public Locator getWebsiteField() {
+//        return websiteField;
+//    }
+//
+//    public Locator getPrimaryContactField() {
+//        return primaryContactField;
+//    }
+//
+//    public Locator getCompanyEmailField() {
+//        return companyEmailField;
+//    }
+//
+//    public Locator getCountryField() {
+//        return countryField;
+//    }
+//
+//    public Locator getStateField() {
+//        return stateField;
+//    }
+//
+//    public Locator getZipField() {
+//        return zipField;
+//    }
+//
+//    public Locator getCityField() {
+//        return cityField;
+//    }
+//
+//    public Locator getPhoneField() {
+//        return phoneField;
+//    }
+//
+//    public Locator getMobileField() {
+//        return mobileField;
+//    }
+//
+//    public Locator getFaxField() {
+//        return faxField;
+//    }
 }
--- src/test/java/xyz/npgw/test/run/AddCompanyDialogTest.java
+++ src/test/java/xyz/npgw/test/run/AddCompanyDialogTest.java
@@ -1,5 +1,6 @@
 package xyz.npgw.test.run;
 
+import com.microsoft.playwright.Locator;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
@@ -10,7 +11,16 @@ import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.page.AddCompanyDialog;
 import xyz.npgw.test.page.DashboardPage;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 public class AddCompanyDialogTest extends BaseTest {
 
@@ -28,4 +38,87 @@ public class AddCompanyDialogTest extends BaseTest {
         Allure.step("Verify: the header contains the expected title text");
         assertThat(addCompanyPage.getAddCompanyDialogHeader()).hasText("Add company");
     }
+
+    @Test
+    public void testVerifyPlaceholders() {
+        List<String> expectedPlaceholders = List.of(
+                "Enter company name",
+                "Enter type",
+                "Enter company description",
+                "Enter company website",
+                "Enter company primary contact",
+                "Enter company email",
+                "Enter country",
+                "Enter state",
+                "Enter ZIP",
+                "Enter city",
+                "Enter phone",
+                "Enter mobile",
+                "Enter fax"
+        );
+
+        AddCompanyDialog addCompanyPage = new DashboardPage(getPage())
+                .clickSystemAdministrationLink()
+                .clickCompaniesAndBusinessUnitsTabButton()
+                .clickAddCompanyButton();
+
+        Locator inputFields = addCompanyPage.getFieldPlaceholderList();
+
+        List<String> actualPlaceholders = (List<String>) inputFields.evaluateAll(
+                "elements => elements.map(el => el.getAttribute('placeholder'))"
+        );
+
+        // Сравниваем списки напрямую
+        assertEquals(expectedPlaceholders, actualPlaceholders);
+    }
+//        System.out.println("Количество найденных полей: " + inputFields.count());
+//        // Извлекаем все плейсхолдеры в список
+//        List<String> actualPlaceholders = inputFields.all()
+//                .stream()
+//                .map(input -> input.getAttribute("placeholder"))
+//                .toList();
+//
+//     actualPlaceholders.forEach(System.out::println);
+
+    // Проверяем, что извлеченные плейсхолдеры соответствуют ожидаемым
+    // assertThat(actualPlaceholders).containsExactly(expectedPlaceholders);
 }
+
+//        Map<Locator, String> fields = new HashMap<>();
+//        fields.put(addCompanyPage.getCompanyNameField(), "Enter company name");
+//        fields.put(addCompanyPage.getCompanyTypeField(), "Enter type");
+//        fields.put(addCompanyPage.getDescriptionField(), "Enter company description");
+//        fields.put(addCompanyPage.getWebsiteField(), "Enter company website");
+//        fields.put(addCompanyPage.getPrimaryContactField(), "Enter company primary contact");
+//        fields.put(addCompanyPage.getCompanyEmailField(), "Enter company email");
+//        fields.put(addCompanyPage.getCountryField(), "Enter country");
+//        fields.put(addCompanyPage.getStateField(), "Enter state");
+//        fields.put(addCompanyPage.getZipField(), "Enter ZIP");
+//        fields.put(addCompanyPage.getCityField(), "Enter city");
+//        fields.put(addCompanyPage.getPhoneField(), "Enter phone");
+//        fields.put(addCompanyPage.getMobileField(), "Enter mobile");
+//        fields.put(addCompanyPage.getFaxField(), "Enter fax");
+//
+//        for (Map.Entry<Locator, String> entry : fields.entrySet()) {
+//            Locator field = entry.getKey();
+//            String expectedPlaceholder = entry.getValue();
+//
+//            assertThat(field).hasAttribute("placeholder", expectedPlaceholder);
+//        }
+
+//        assertThat(addCompanyPage.getCompanyNameField()).hasAttribute("placeholder", "Enter company name");
+//        assertThat(addCompanyPage.getCompanyTypeField()).hasAttribute("placeholder", "Enter type");
+//        assertThat(addCompanyPage.getDescriptionField()).hasAttribute("placeholder", "Enter company description");
+//        assertThat(addCompanyPage.getWebsiteField()).hasAttribute("placeholder", "Enter company website");
+//        assertThat(addCompanyPage.getPrimaryContactField()).hasAttribute("placeholder", "Enter company primary contact");
+//        assertThat(addCompanyPage.getCompanyEmailField()).hasAttribute("placeholder", "Enter company email");
+//        assertThat(addCompanyPage.getCountryField()).hasAttribute("placeholder", "Enter country");
+//        assertThat(addCompanyPage.getStateField()).hasAttribute("placeholder", "Enter state");
+//        assertThat(addCompanyPage.getZipField()).hasAttribute("placeholder", "Enter ZIP");
+//        assertThat(addCompanyPage.getCityField()).hasAttribute("placeholder", "Enter city");
+//        assertThat(addCompanyPage.getPhoneField()).hasAttribute("placeholder", "Enter phone");
+//        assertThat(addCompanyPage.getMobileField()).hasAttribute("placeholder", "Enter mobile");
+//        assertThat(addCompanyPage.getFaxField()).hasAttribute("placeholder", "Enter fax");
+//    }
+//    }
+//}
