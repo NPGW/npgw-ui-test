f3841c7073bf06841c286eae51c71259ab2ee9b2
Laysan Nova <laysansh@gmail.com>
2025-04-04 12:00:54 -0600

    commit

--- src/test/java/xyz/npgw/test/page/systemadministration/AcquirersPage.java
+++ src/test/java/xyz/npgw/test/page/systemadministration/AcquirersPage.java
@@ -1,21 +1,28 @@
-package xyz.npgw.test.page.systemadministration;
+package xyz.npgw.test.page;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
-import xyz.npgw.test.page.base.SystemAdministrationBasePage;
+import xyz.npgw.test.page.base.BasePageWithHeader;
 
-public class AcquirersPage extends SystemAdministrationBasePage {
+public class AcquirersPage extends BasePageWithHeader {
 
     private final Locator addAcquirerButton = locator("svg[data-icon='circle-plus']");
     private final Locator resetFilterButton = locator("svg[data-icon='xmark']");
     private final Locator applyFilterButton = locator("svg[data-icon='filter']");
-    private final Locator selectAcquirerLabel = labelExact("Select acquirer");
-    private final Locator statusLabel = labelExact("Status");
+
     private final Locator acquirersListHeader = textExact("Acquirers list");
     private final Locator acquirersList = locator("div[data-slot='base'] li");
+
+    private final Locator selectAcquirerLabel = labelExact("Select acquirer");
     private final Locator selectAcquirerPlaceholder = placeholder("Search");
     private final Locator dropdownAcquirerList = locator("div[data-slot='content'] li");
+
+    private final Locator acquirerStatusLabel = labelExact("Status");
+    private final Locator acquirerStatusPlaceholder = locator("div[data-slot='innerWrapper'] span");
+    private final Locator dropdownAcquirerStatusList = locator("div[data-slot='listbox']");
+    private final Locator acquirerStatusOptions = option(dropdownAcquirerStatusList);
+
 
     public AcquirersPage(Page page) {
         super(page);
@@ -38,7 +45,7 @@ public class AcquirersPage extends SystemAdministrationBasePage {
     }
 
     public Locator getStatusLabel() {
-        return statusLabel;
+        return acquirerStatusLabel;
     }
 
     public Locator getAcquirersListHeader() {
@@ -64,4 +71,23 @@ public class AcquirersPage extends SystemAdministrationBasePage {
 
         return dropdownAcquirerList;
     }
+
+    @Step("Click Status placeholder")
+    public AcquirersPage clickAcquirerStatusPlaceholder() {
+        selectAcquirerLabel.waitFor();
+        acquirerStatusPlaceholder.click();
+
+        return this;
+    }
+
+    public Locator getDropdownAcquirerStatusList() {
+
+        return dropdownAcquirerStatusList;
+    }
+
+    public Locator getAcquirerStatusOptions() {
+
+        return acquirerStatusOptions;
+    }
+
 }
--- src/test/java/xyz/npgw/test/run/AcquirersPageTest.java
+++ src/test/java/xyz/npgw/test/run/AcquirersPageTest.java
@@ -9,8 +9,9 @@ import io.qameta.allure.TmsLink;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
+import xyz.npgw.test.page.AcquirersPage;
 import xyz.npgw.test.page.DashboardPage;
-import xyz.npgw.test.page.systemadministration.AcquirersPage;
+import xyz.npgw.test.testdata.StatusOption;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -23,26 +24,26 @@ public class AcquirersPageTest extends BaseTest {
     @Feature("Acquirers list")
     @Description("Verify: The visibility of elements in the 'Acquirers List' control panel")
     public void testVisibilityAcquirersListControlTab() {
-        AcquirersPage saAcquirersTab = new DashboardPage(getPage())
+        AcquirersPage acquirersPage = new DashboardPage(getPage())
                 .getHeader()
                 .clickSystemAdministrationLink()
                 .getSystemAdministrationMenuComponent()
                 .clickAcquirersTab();
 
         Allure.step("Verify: Add Acquirer Button is visible");
-        assertThat(saAcquirersTab.getAddAcquirerButton()).isVisible();
+        assertThat(acquirersPage.getAddAcquirerButton()).isVisible();
 
         Allure.step("Verify: Select Acquirer label is visible");
-        assertThat(saAcquirersTab.getSelectAcquirerLabel()).isVisible();
+        assertThat(acquirersPage.getSelectAcquirerLabel()).isVisible();
 
         Allure.step("Verify: Status label is visible");
-        assertThat(saAcquirersTab.getStatusLabel()).isVisible();
+        assertThat(acquirersPage.getStatusLabel()).isVisible();
 
         Allure.step("Verify: Reset Filter Button is visible");
-        assertThat(saAcquirersTab.getResetFilterButton()).isVisible();
+        assertThat(acquirersPage.getResetFilterButton()).isVisible();
 
         Allure.step("Verify: Apply Filter Button is visible");
-        assertThat(saAcquirersTab.getApplyFilterButton()).isVisible();
+        assertThat(acquirersPage.getApplyFilterButton()).isVisible();
     }
 
     @Test
@@ -51,16 +52,16 @@ public class AcquirersPageTest extends BaseTest {
     @Feature("Acquirers list")
     @Description("Verify: The visibility of the 'Acquirers List' header, which contains a list of Acquirers.")
     public void testVisibilityHeaderAndAcquirersList() {
-        AcquirersPage saAcquirersTab = new DashboardPage(getPage())
+        AcquirersPage acquirersPage = new DashboardPage(getPage())
                 .getHeader()
                 .clickSystemAdministrationLink()
                 .getSystemAdministrationMenuComponent()
                 .clickAcquirersTab();
 
         Allure.step("Verify: Acquirers list header is visible");
-        assertThat(saAcquirersTab.getAcquirersListHeader()).isVisible();
+        assertThat(acquirersPage.getAcquirersListHeader()).isVisible();
 
-        Locator acquirersList = saAcquirersTab.getAcquirersList();
+        Locator acquirersList = acquirersPage.getAcquirersList();
 
         Allure.step(String.format(
                 "Verify: Acquirers list is visible and contains elements. INFO: (%d elements)", acquirersList.count()));
@@ -85,5 +86,39 @@ public class AcquirersPageTest extends BaseTest {
         Allure.step(String.format(
                 "Verify: Dropdown list is not empty. INFO: (%d elements)", dropdownAcquirerList.count()));
         assertThat(dropdownAcquirerList).not().hasCount(0);
+    }
+
+    @Test
+    @TmsLink("187")
+    @Epic("SA/Acquirers")
+    @Feature("Status")
+    @Description("Verify: The 'Status' dropdown toggles and contains options All, Active, Inactive.")
+    public void testOpenStatusDropdown() {
+        AcquirersPage acquirersPage = new DashboardPage(getPage())
+                .getHeader()
+                .clickSystemAdministrationLink()
+                .getSystemAdministrationMenuComponent()
+                .clickAcquirersTab();
+
+        Locator dropdownStatusList = acquirersPage
+                .clickAcquirerStatusPlaceholder()
+                .getDropdownAcquirerStatusList();
+
+        Allure.step("Verify: Dropdown acquirer status list is visible");
+        assertThat(dropdownStatusList).isVisible();
+
+        Locator actualOptions = acquirersPage.getAcquirerStatusOptions();
+
+        Allure.step("Verify: Dropdown acquirer status list has 3 options");
+        assertThat(actualOptions).hasCount(StatusOption.values().length);
+
+        Allure.step("Verify: Option 1 has value 'All'");
+        assertThat(actualOptions.nth(0)).hasValue(StatusOption.ALL.getValue());
+
+        Allure.step("Verify: Option 2 has value 'Active'");
+        assertThat(actualOptions.nth(0)).hasValue(StatusOption.ACTIVE.getValue());
+
+        Allure.step("Verify: Option 3 has value 'Inactive'");
+        assertThat(actualOptions.nth(0)).hasValue(StatusOption.INACTIVE.getValue());
     }
 }
