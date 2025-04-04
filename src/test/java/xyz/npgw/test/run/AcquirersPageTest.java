5182c8ee923a014877616518a9133dbb227d151a
Laysan Nova <laysansh@gmail.com>
2025-04-04 12:16:16 -0600

    commit

--- src/test/java/xyz/npgw/test/run/AcquirersPageTest.java
+++ src/test/java/xyz/npgw/test/run/AcquirersPageTest.java
@@ -6,11 +6,11 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.page.DashboardPage;
 import xyz.npgw.test.page.systemadministration.AcquirersPage;
+import xyz.npgw.test.testdata.StatusOption;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -50,16 +50,16 @@ public class AcquirersPageTest extends BaseTest {
     @Feature("Acquirers list")
     @Description("Verify: The visibility of the 'Acquirers List' header, which contains a list of Acquirers.")
     public void testVisibilityHeaderAndAcquirersList() {
-        AcquirersPage saAcquirersTab = new DashboardPage(getPage())
+        AcquirersPage acquirersPage = new DashboardPage(getPage())
                 .getHeader()
                 .clickSystemAdministrationLink()
                 .getSystemAdministrationMenuComponent()
                 .clickAcquirersTab();
 
         Allure.step("Verify: Acquirers list header is visible");
-        assertThat(saAcquirersTab.getAcquirersListHeader()).isVisible();
+        assertThat(acquirersPage.getAcquirersListHeader()).isVisible();
 
-        Locator acquirersList = saAcquirersTab.getAcquirersList();
+        Locator acquirersList = acquirersPage.getAcquirersList();
 
         Allure.step(String.format(
                 "Verify: Acquirers list is visible and contains elements. INFO: (%d elements)", acquirersList.count()));
@@ -84,5 +84,39 @@ public class AcquirersPageTest extends BaseTest {
         Allure.step(String.format(
                 "Verify: Dropdown list is not empty. INFO: (%d elements)", dropdownAcquirerList.count()));
         assertThat(dropdownAcquirerList).not().hasCount(0);
+    }
+
+    @Test
+    @TmsLink("187")
+    @Epic("SA/Acquirers")
+    @Feature("Status")
+    @Description("Verify: The 'Status' dropdown toggles and contains options All, Active, Inactive.")
+    public void testOpenStatusDropdown() {
+        AcquirersPage acquirersPage = new DashboardPage(getPage())
+                .getHeader()
+                .clickSystemAdministrationLink()
+                .getSystemAdministrationMenuComponent()
+                .clickAcquirersTab();
+
+        Locator dropdownStatusList = acquirersPage
+                .clickAcquirerStatusPlaceholder()
+                .getDropdownAcquirerStatusList();
+
+        Allure.step("Verify: Dropdown acquirer status list is visible");
+        assertThat(dropdownStatusList).isVisible();
+
+        Locator actualOptions = acquirersPage.getAcquirerStatusOptions();
+
+        Allure.step("Verify: Dropdown acquirer status list has 3 options");
+        assertThat(actualOptions).hasCount(StatusOption.values().length);
+
+        Allure.step("Verify: Option 1 has value 'All'");
+        assertThat(actualOptions.nth(0)).hasValue(StatusOption.ALL.getValue());
+
+        Allure.step("Verify: Option 2 has value 'Active'");
+        assertThat(actualOptions.nth(1)).hasValue(StatusOption.ACTIVE.getValue());
+
+        Allure.step("Verify: Option 3 has value 'Inactive'");
+        assertThat(actualOptions.nth(2)).hasValue(StatusOption.INACTIVE.getValue());
     }
 }
