c4d1d90ad5cb4678444b5f0170efa9c0633bec29
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-04-07 09:14:36 +0300

    Fix: rm import

--- src/test/java/xyz/npgw/test/page/systemadministration/TeamPage.java
+++ src/test/java/xyz/npgw/test/page/systemadministration/TeamPage.java
@@ -2,7 +2,6 @@ package xyz.npgw.test.page.systemadministration;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.LoadState;
 import io.qameta.allure.Step;
 import xyz.npgw.test.page.base.SystemAdministrationWithTableBasePage;
 
