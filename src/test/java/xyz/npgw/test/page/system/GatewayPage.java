319c22807736ad1bc2574fc485c84dfc80b5b2e9
Laysan Nova <laysansh@gmail.com>
2025-04-14 20:50:40 -0600

    add 12.002.02

--- src/test/java/xyz/npgw/test/page/system/GatewayPage.java
+++ src/test/java/xyz/npgw/test/page/system/GatewayPage.java
@@ -2,12 +2,14 @@ package xyz.npgw.test.page.system;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import io.qameta.allure.Step;
 import lombok.Getter;
 
 public class GatewayPage extends BaseSystemPage {
 
     private final Locator currencyLabel = labelExact("Currency");
+    @Getter
     private final Locator currencyPlaceholder = locator("div[data-slot='innerWrapper'] span");
     private final Locator dropdownCurrencyList = locator("div[data-slot='listbox']");
     @Getter
@@ -22,6 +24,15 @@ public class GatewayPage extends BaseSystemPage {
     public GatewayPage clickCurrencyPlaceholder() {
         currencyLabel.waitFor();
         currencyPlaceholder.click();
+
+        return this;
+    }
+
+    @Step("Select Currency '{currency}'")
+    public GatewayPage selectCurrency(String currency) {
+        Locator option = getPage().locator("li[data-key='" + currency.toUpperCase() + "']");
+        option.click();
+        dropdownCurrencyList.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.HIDDEN));
 
         return this;
     }
--- src/test/java/xyz/npgw/test/run/GatewayPageTest.java
+++ src/test/java/xyz/npgw/test/run/GatewayPageTest.java
@@ -6,9 +6,13 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.page.DashboardPage;
+import xyz.npgw.test.page.system.GatewayPage;
+
+import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -30,5 +34,39 @@ public class GatewayPageTest extends BaseTest {
 
         Allure.step("Verify: The 'Currency' dropdown toggles and contains options All, USD, EUR.");
         assertThat(actualOptions).hasText(new String[]{"ALL", "USD", "EUR"});
+    }
+
+    @Ignore
+    @Test
+    @TmsLink("285")
+    @Epic("System/Gateway")
+    @Feature("Currency")
+    @Description("Verify that re-selecting an already selected currency keeps the selection unchanged.")
+    public void testRetainCurrencyWhenReSelectingSameOption() {
+
+        List<String> expectedOptions = List.of("All", "USD", "EUR");
+
+        GatewayPage gatewayPage = new DashboardPage(getPage())
+                .getHeader()
+                .clickSystemAdministrationLink()
+                .getSystemMenu()
+                .clickGatewayTab();
+
+        for (String currency : expectedOptions) {
+            Locator actualCurrency = gatewayPage
+                    .clickCurrencyPlaceholder()
+                    .selectCurrency(currency)
+                    .getCurrencyPlaceholder();
+
+            Allure.step("Verify placeholder matches expected value: " + currency);
+            assertThat(actualCurrency).hasText(currency);
+
+            gatewayPage.clickCurrencyPlaceholder()
+                    .selectCurrency(currency)
+                    .getCurrencyPlaceholder();
+
+            Allure.step("Verify again placeholder matches expected value: " + currency);
+            assertThat(actualCurrency).hasText(currency);
+        }
     }
 }
