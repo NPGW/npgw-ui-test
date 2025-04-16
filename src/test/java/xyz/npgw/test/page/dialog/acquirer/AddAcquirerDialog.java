19b52e676a21398730e8f18425f20cdd7b4ed69d
Serhii Malko <malsv@ukr.net>
2025-04-16 17:56:02 +0300

    after review code analysis

--- src/test/java/xyz/npgw/test/page/dialog/acquirer/AddAcquirerDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/acquirer/AddAcquirerDialog.java
@@ -2,17 +2,13 @@ package xyz.npgw.test.page.dialog.acquirer;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.WaitForSelectorState;
 import io.qameta.allure.Step;
 import lombok.Getter;
 import xyz.npgw.test.page.system.AcquirersPage;
-import xyz.npgw.test.page.system.CompaniesAndBusinessUnitsPage;
 
-import java.util.List;
-
+@Getter
 public class AddAcquirerDialog extends AcquirerDialog<AddAcquirerDialog> {
 
-    @Getter
     private final Locator createButton = buttonByName("Create");
 
     public AddAcquirerDialog(Page page) {
--- src/test/java/xyz/npgw/test/page/dialog/acquirer/EditAcquirerDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/acquirer/EditAcquirerDialog.java
@@ -1,8 +1,6 @@
 package xyz.npgw.test.page.dialog.acquirer;
 
-import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import io.qameta.allure.Step;
 
 public class EditAcquirerDialog extends AcquirerDialog<EditAcquirerDialog> {
 
