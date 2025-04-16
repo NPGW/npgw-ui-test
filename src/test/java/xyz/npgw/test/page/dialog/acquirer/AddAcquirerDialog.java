996585d3c6d3c00bbf9d0f698f9b1f0a6791523c
Serhii Malko <malsv@ukr.net>
2025-04-16 17:53:40 +0300

    add generic to the acquirers dialogs

--- src/test/java/xyz/npgw/test/page/dialog/acquirer/AddAcquirerDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/acquirer/AddAcquirerDialog.java
@@ -6,48 +6,25 @@ import com.microsoft.playwright.options.WaitForSelectorState;
 import io.qameta.allure.Step;
 import lombok.Getter;
 import xyz.npgw.test.page.system.AcquirersPage;
+import xyz.npgw.test.page.system.CompaniesAndBusinessUnitsPage;
 
 import java.util.List;
 
-public class AddAcquirerDialog extends AcquirerDialog {
+public class AddAcquirerDialog extends AcquirerDialog<AddAcquirerDialog> {
 
     @Getter
-    private final Locator addAcquirerDialogHeader = locator("section header");
-    private final Locator allFieldPlaceholders = locator("[data-slot='input']:not([placeholder='Search...'])");
-    @Getter
-    private final Locator statusSwitch = locator("div[role='radiogroup']");
-    @Getter
-    private final Locator allowedCurrenciesCheckboxes = locator("div[role='group']");
-    @Getter
-    private final Locator selectCountryPlaceholder = labelExact("Select country");
-    @Getter
-    private final Locator selectTimezone = textExact("Select timezone");
+    private final Locator createButton = buttonByName("Create");
 
     public AddAcquirerDialog(Page page) {
         super(page);
     }
 
-    public List<String> getAllFieldPlaceholders() {
-        allFieldPlaceholders.first().waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
-
-        return allFieldPlaceholders.all().stream().map(l -> l.getAttribute("placeholder")).toList();
-    }
-
-    @Step("Click 'Close' button")
-    public AcquirersPage clickCloseButton() {
-        getCloseButton().click();
+    @Step("Click on the 'Create' button")
+    public AcquirersPage clickCreateButton() {
+        createButton.click();
 
         return new AcquirersPage(getPage());
     }
 
-    @Step("Click '{option}' radiobutton")
-    public AddAcquirerDialog clickStatusRadiobutton(String option) {
-        labelExact(option).click();
 
-        return this;
-    }
-
-    public Locator getStatusRadiobutton(String value) {
-        return statusSwitch.locator("label:has(input[value='" + value.toUpperCase() + "'])");
-    }
 }
