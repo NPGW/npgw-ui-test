49664741487835e6e6a161717eed52f61000eeff
Laysan Nova <laysansh@gmail.com>
2025-04-17 01:56:40 -0600

    add ATC_12.003.01

--- src/test/java/xyz/npgw/test/page/system/GatewayPage.java
+++ src/test/java/xyz/npgw/test/page/system/GatewayPage.java
@@ -2,11 +2,39 @@ package xyz.npgw.test.page.system;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.Position;
 import com.microsoft.playwright.options.WaitForSelectorState;
 import io.qameta.allure.Step;
 import lombok.Getter;
+import org.testng.Assert;
 
 public class GatewayPage extends BaseSystemPage {
+
+
+    @Getter
+    private final Locator businessUnitsListHeader = textExact("Business units list");
+    @Getter
+    private final Locator businessUnitsList = locator("div[data-slot='base'] li");
+
+    @Getter
+    private final Locator selectCompanyPlaceholder = locator("input[aria-label='Select company']");
+
+    // selectCompanyClearIcon
+    /* Хотела так
+
+    private final Locator selectCompanyContainer = locator("div[data-slot='input-wrapper']")
+            .filter(new Locator.FilterOptions()
+            .setHas(selectCompanyPlaceholder));
+    private final Locator selectCompanyClearIcon = selectCompanyContainer
+        .locator("button[aria-label='Show suggestions']:first-child");
+    */
+
+    // А так временно сделала - копирует вариант выше просто написано все вместе
+    private final Locator selectCompanyClearIcon = locator("div[data-slot='input-wrapper']:has(input[aria-label='Select company']) button[aria-label='Show suggestions']:first-child").locator("..");
+
+    @Getter
+    private final Locator companyDropdown = locator("div[data-slot='content']");
+    private final Locator lastOptionCompanyDropdown = companyDropdown.locator("li");
 
     private final Locator currencyLabel = labelExact("Currency");
     @Getter
@@ -36,4 +64,53 @@ public class GatewayPage extends BaseSystemPage {
 
         return this;
     }
+
+    @Step("Click 'Select company' placeholder")
+    public GatewayPage clickSelectCompanyPlaceholder() {
+        businessUnitsListHeader.waitFor();
+        selectCompanyPlaceholder.click();
+
+        return this;
+    }
+
+    @Step("Click company '{company}' in dropdown")
+    public GatewayPage clickCompanyInDropdown(String company) {
+
+        boolean found = false;
+        String lastOptionBeforeScroll = "";
+
+        while (!found) {
+            String currentLastOptionText = lastOptionCompanyDropdown.last().textContent();
+
+            if (lastOptionBeforeScroll.equals(currentLastOptionText) && !lastOptionBeforeScroll.isEmpty()) {
+                Assert.fail("Company '" + company + "' not found in dropdown.");
+                break;
+            }
+
+            lastOptionBeforeScroll = currentLastOptionText;
+
+            Locator matchingOption = lastOptionCompanyDropdown
+                    .filter(new Locator.FilterOptions().setHasText(company));
+
+            if (matchingOption.count() > 0 && matchingOption.first().isVisible()) {
+                found = true;
+                matchingOption.click();
+            } else {
+                lastOptionCompanyDropdown.last().scrollIntoViewIfNeeded();
+                getPage().waitForTimeout(200);
+            }
+        }
+
+        return this;
+    }
+
+    @Step("Click select Company clear icon")
+    public GatewayPage clickSelectCompanyClearIcon() {
+        getPage().waitForTimeout(2000); //Временно добавлено
+        selectCompanyClearIcon.click();
+        businessUnitsListHeader.click();
+
+        return this;
+    }
 }
+
