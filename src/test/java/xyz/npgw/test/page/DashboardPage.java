9ee00c55f3da2e9d949521afd260b69b68c327eb
AllaBird <allavorobieva@yahoo.com>
2025-04-23 01:54:13 -0400

    [ATC-02.001.00]

--- src/test/java/xyz/npgw/test/page/DashboardPage.java
+++ src/test/java/xyz/npgw/test/page/DashboardPage.java
@@ -1,11 +1,53 @@
 package xyz.npgw.test.page;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import io.qameta.allure.Step;
+import lombok.Getter;
 import xyz.npgw.test.page.common.HeaderPage;
 
 public final class DashboardPage extends HeaderPage {
 
+    private final Locator dateRange = getPage().getByRole(AriaRole.SPINBUTTON);
+    private final Locator applyFilter = locator("[data-icon='filter']");
+    @Getter
+    private final Locator dataRangeErrorMessage = locator("[data-slot='error-message']");
+
     public DashboardPage(Page page) {
         super(page);
+    }
+
+    @Step("Set start date: {startDate}")
+    public DashboardPage setStartDate(String startDate) {
+        Locator day = dateRange.nth(0);
+        Locator month = dateRange.nth(1);
+        Locator year = dateRange.nth(2);
+
+        day.fill(startDate.split("-")[0]);
+        month.fill(startDate.split("-")[1]);
+        year.fill(startDate.split("-")[2]);
+
+        return this;
+    }
+
+    @Step("Set end date: {endDate}")
+    public DashboardPage setEndDate(String endDate) {
+        Locator day = dateRange.nth(3);
+        Locator month = dateRange.nth(4);
+        Locator year = dateRange.nth(5);
+
+        day.fill(endDate.split("-")[0]);
+        month.fill(endDate.split("-")[1]);
+        year.fill(endDate.split("-")[2]);
+
+        return this;
+    }
+
+    @Step("Click 'Apply filter")
+    public DashboardPage clickApplyFilter() {
+        applyFilter.click();
+
+        return this;
     }
 }
