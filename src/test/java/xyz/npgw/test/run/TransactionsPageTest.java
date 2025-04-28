a3a3b95abe9cc2362e741e6ae45fc10295b7cce7
AllaBird <allavorobieva@yahoo.com>
2025-04-27 21:41:48 -0400

    Add DataRangePickerComponent

--- src/test/java/xyz/npgw/test/run/TransactionsPageTest.java
+++ src/test/java/xyz/npgw/test/run/TransactionsPageTest.java
@@ -139,7 +139,7 @@ public class TransactionsPageTest extends BaseTest {
                 .clickTransactionsLink();
 
         Allure.step("Verify: DataRange picker is visible");
-        assertThat(transactionsPage.getDateRangePicker()).isVisible();
+        assertThat(transactionsPage.getDateRangePicker().getDateRangePicker()).isVisible();
 
         Allure.step("Verify: Business Unit selector is visible");
         assertThat(transactionsPage.getBusinessUnitSelector()).isVisible();
@@ -302,12 +302,13 @@ public class TransactionsPageTest extends BaseTest {
         TransactionsPage transactionsPage = new DashboardPage(getPage())
                 .getHeader()
                 .clickTransactionsLink()
-                .setStartDate("01-04-2025")
-                .setEndDate("01-04-2024")
+                .getDateRangePicker()
+                .setDateRangeFields("01-04-2025", "01-04-2024")
                 .clickRefreshDataButton();
 
         Allure.step("Verify: error message is shown for invalid date range");
-        assertThat(transactionsPage.getDataRangeErrorMessage()).hasText("Start date must be before end date.");
+        assertThat(transactionsPage.getDateRangePicker().getDataRangePickerErrorMessage()).hasText(
+                "Start date must be before end date.");
     }
 
     @Test
