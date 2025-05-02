3d21b57347d1d1ce611382793a279814872c774e
Serhii Malko <malsv@ukr.net>
2025-05-02 14:00:53 +0300

    fix testFilterTransactionsByCurrency (parts)

--- src/test/java/xyz/npgw/test/run/AddBusinessUnitTest.java
+++ src/test/java/xyz/npgw/test/run/AddBusinessUnitTest.java
@@ -7,7 +7,6 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import net.datafaker.Faker;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.TestUtils;
 import xyz.npgw.test.common.base.BaseTest;
