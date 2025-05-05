cbccc5e5d03b928104fe760b79d1f19f6ee8df5e
Laysan Nova <laysansh@gmail.com>
2025-05-04 20:53:31 -0600

    add ATC_11.002.01

--- src/test/java/xyz/npgw/test/run/EditBusinessUnitDialogTest.java
+++ src/test/java/xyz/npgw/test/run/EditBusinessUnitDialogTest.java
@@ -16,7 +16,6 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class EditBusinessUnitDialogTest extends BaseTest {
 
-    @Ignore
     @Test
     @TmsLink("387")
     @Epic("System/Companies and business units")
