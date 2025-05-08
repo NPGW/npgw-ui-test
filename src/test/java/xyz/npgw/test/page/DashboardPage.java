ff1475f31ba3efdfee31ce196c67ac0546b5fe53
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2025-05-08 14:45:44 +0500

    Style check improvements

--- src/test/java/xyz/npgw/test/page/DashboardPage.java
+++ src/test/java/xyz/npgw/test/page/DashboardPage.java
@@ -3,8 +3,8 @@ package xyz.npgw.test.page;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
-import xyz.npgw.test.page.common.DateRangePickerTrait;
 import xyz.npgw.test.page.common.AlertTrait;
+import xyz.npgw.test.page.common.DateRangePickerTrait;
 import xyz.npgw.test.page.common.HeaderPage;
 
 public final class DashboardPage extends HeaderPage implements DateRangePickerTrait<DashboardPage>,
