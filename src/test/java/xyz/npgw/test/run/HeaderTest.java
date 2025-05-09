bdfe513304bc2bb289a9b34656d9c6b06a41a65e
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2025-05-09 12:35:33 +0500

    Change of method to universal light+dark

--- src/test/java/xyz/npgw/test/run/HeaderTest.java
+++ src/test/java/xyz/npgw/test/run/HeaderTest.java
@@ -1,6 +1,6 @@
 package xyz.npgw.test.run;
 
-import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.ColorScheme;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
@@ -178,7 +178,12 @@ public class HeaderTest extends BaseTest {
         new LoginPage(getPage())
                 .loginAs(UserRole.SUPER);
 
-        Allure.step("Verify that the current color theme is dark, matching the browser's theme");
-        assertThat(getPage().locator("html.dark")).isVisible();
+        Allure.step("Verify that the current color theme matches the default browser theme");
+        ColorScheme expectedTheme = ProjectProperties.getColorScheme();
+        if (expectedTheme == ColorScheme.DARK) {
+            assertThat(getPage().locator("html.dark")).isVisible();
+        } else if (expectedTheme == ColorScheme.LIGHT) {
+            assertThat(getPage().locator("html.light")).isVisible();
+        }
     }
 }
