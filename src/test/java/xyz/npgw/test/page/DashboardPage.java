9d0045aeb6c94fa78d2eabfe56071d393f5a102d
AllaBird <allavorobieva@yahoo.com>
2025-05-16 23:44:52 -0400

    02.005.00

--- src/test/java/xyz/npgw/test/page/DashboardPage.java
+++ src/test/java/xyz/npgw/test/page/DashboardPage.java
@@ -2,16 +2,23 @@ package xyz.npgw.test.page;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.Step;
+import lombok.Getter;
 import xyz.npgw.test.page.common.AlertTrait;
 import xyz.npgw.test.page.common.DateRangePickerTrait;
 import xyz.npgw.test.page.common.HeaderPage;
 import xyz.npgw.test.page.common.SelectBusinessUnitTrait;
+import xyz.npgw.test.page.common.SelectCompanyTrait;
 
 public final class DashboardPage extends HeaderPage implements DateRangePickerTrait<DashboardPage>,
-        AlertTrait<DashboardPage>, SelectBusinessUnitTrait<DashboardPage> {
+        AlertTrait<DashboardPage>, SelectBusinessUnitTrait<DashboardPage>,
+        SelectCompanyTrait<DashboardPage> {
 
     private final Locator refreshDataButton = locator("[data-icon='arrows-rotate']");
+    private final Locator resetFilterButton = getByTestId("ResetFilterButtonDashboardPage");
+    @Getter
+    private final Locator currencySelector = getByLabelExact("Currency");
 
     public DashboardPage(Page page) {
         super(page);
@@ -27,6 +34,28 @@ public final class DashboardPage extends HeaderPage implements DateRangePickerTr
     @Step("Reload dashboard page")
     public DashboardPage reloadDashboard() {
         getPage().reload();
+
+        return this;
+    }
+
+    @Step("Click 'Reset filter' button")
+    public DashboardPage clickResetFilterButton() {
+        resetFilterButton.click();
+
+        return this;
+    }
+
+    @Step("Click Currency Selector")
+    public DashboardPage clickCurrencySelector() {
+        currencySelector.click();
+
+        return this;
+    }
+
+    @Step("Select currency from dropdown menu")
+    public DashboardPage selectCurrency(String value) {
+        getByRole(AriaRole.OPTION, value).click();
+        getPage().locator("h2").filter(new Locator.FilterOptions().setHasText(value)).first().waitFor();
 
         return this;
     }
--- src/test/java/xyz/npgw/test/run/DashboardPageTest.java
+++ src/test/java/xyz/npgw/test/run/DashboardPageTest.java
@@ -5,13 +5,14 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.Constants;
 import xyz.npgw.test.common.base.BaseTest;
+import xyz.npgw.test.common.util.TestUtils;
 import xyz.npgw.test.page.DashboardPage;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.testng.Assert.assertTrue;
 
 public class DashboardPageTest extends BaseTest {
 
@@ -30,7 +31,6 @@ public class DashboardPageTest extends BaseTest {
         assertThat(dashboardPage.getPage()).hasTitle(Constants.DASHBOARD_URL_TITLE);
     }
 
-//    @Ignore("Не нажимается рефреш - снять игнор после фикса /summary")
     @Test
     @TmsLink("403")
     @Epic("Dashboard")
@@ -45,5 +45,29 @@ public class DashboardPageTest extends BaseTest {
         Allure.step("Verify: error message is shown for invalid date range");
         assertThat(dashboardPage.getDateRangePicker().getDataRangePickerErrorMessage()).hasText(
                 "Start date must be before end date.");
+    }
+
+
+    // TODO: Add business unit check when enabled
+    @Test
+    @TmsLink("577")
+    @Epic("Dashboard")
+    @Feature("Reset filter")
+    @Description("'Reset filter' clears selected options to default")
+    public void testResetFilter() {
+        final String companyName = "framework";
+        TestUtils.createCompanyIfNeeded(getApiRequestContext(), companyName);
+
+        DashboardPage dashboardPage = new DashboardPage(getPage())
+                .getSelectCompany().selectCompany(companyName)
+                .clickCurrencySelector()
+                .selectCurrency("EUR")
+                .clickResetFilterButton();
+
+        Allure.step("Verify: the selected company field is empty after reset");
+        assertTrue(dashboardPage.getSelectCompany().getSelectCompanyField().inputValue().isEmpty());
+
+        Allure.step("Verify: the currency selector displays 'ALL' after reset");
+        assertThat(dashboardPage.getCurrencySelector()).containsText("ALL");
     }
 }
