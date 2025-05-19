521932ae28ccf31e874de6453a0978feba7a646d
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-05-20 00:36:41 +0300

    one more run

--- src/test/java/xyz/npgw/test/common/state/State.java
+++ src/test/java/xyz/npgw/test/common/state/State.java
@@ -8,6 +8,7 @@ import java.time.LocalTime;
 @Setter
 @Getter
 class State {
+
     private LocalTime superExpiration = LocalTime.now().minusMinutes(1);
     private LocalTime adminExpiration = LocalTime.now().minusMinutes(1);
     private LocalTime userExpiration = LocalTime.now().minusMinutes(1);
