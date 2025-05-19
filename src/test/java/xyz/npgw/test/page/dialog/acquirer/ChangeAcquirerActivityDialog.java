3ea2f5593b957a67d29ec49e64bcb79f80562390
Laysan Nova <laysansh@gmail.com>
2025-05-19 07:32:44 -0600

    refactoring

--- src/test/java/xyz/npgw/test/page/dialog/acquirer/ChangeAcquirerActivityDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/acquirer/ChangeAcquirerActivityDialog.java
@@ -5,23 +5,20 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.Step;
 import lombok.Getter;
+import xyz.npgw.test.page.base.BaseModel;
+import xyz.npgw.test.page.base.BasePage;
 import xyz.npgw.test.page.dialog.BaseDialog;
 import xyz.npgw.test.page.system.AcquirersPage;
 
 @Getter
-public class ChangeAcquirerActivityDialog<CurrentDialogT extends AcquirerDialog<CurrentDialogT>>
-        extends BaseDialog<AcquirersPage, CurrentDialogT> {
+public class ChangeAcquirerActivityDialog<CurrentDialogT extends BaseDialog<AcquirersPage, CurrentDialogT>>
+        extends BaseModel {
 
     private final Locator deactivateButton = getByRole(AriaRole.BUTTON, "Deactivate");
     private final Locator activateButton = getByRole(AriaRole.BUTTON, "Activate");
 
     public ChangeAcquirerActivityDialog(Page page) {
         super(page);
-    }
-
-    @Override
-    protected AcquirersPage getReturnPage() {
-        return null;
     }
 
     @Step("Click 'Deactivate' button")
