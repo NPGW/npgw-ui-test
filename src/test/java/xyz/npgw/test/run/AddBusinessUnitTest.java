a756f33abf0147c6eb9000d822656b6737f52250
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-05-21 20:19:49 +0300

    one more run

--- src/test/java/xyz/npgw/test/run/AddBusinessUnitTest.java
+++ src/test/java/xyz/npgw/test/run/AddBusinessUnitTest.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import net.datafaker.Faker;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.common.entity.BusinessUnit;
