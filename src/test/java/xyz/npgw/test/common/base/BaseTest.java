f06f699885753ee4bdc1f87b9bba91b5051bdbe0
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-05-21 03:48:57 +0300

    one more run

--- src/test/java/xyz/npgw/test/common/base/BaseTest.java
+++ src/test/java/xyz/npgw/test/common/base/BaseTest.java
@@ -91,7 +91,6 @@ public abstract class BaseTest {
         log.info("{}", getTestName(method, testResult).replace("(", "_").split("_")[0]);
         initApiRequestContext();
         openSite(args);
-        initPageRequestContext();
     }
 
     @AfterMethod
@@ -199,6 +198,7 @@ public abstract class BaseTest {
             }
         }
         new AboutBlankPage(page).navigate("/").loginAs(userRole);
+        initPageRequestContext();
     }
 
     private void initApiRequestContext() {
