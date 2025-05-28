1610f7eb5c12837d4d6c460a020c336f4dc4e7d2
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-05-28 19:53:40 +0300

    EditCompanyDialogTest

--- src/test/java/xyz/npgw/test/run/EditCompanyDialogTest.java
+++ src/test/java/xyz/npgw/test/run/EditCompanyDialogTest.java
@@ -17,13 +17,13 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class EditCompanyDialogTest extends BaseTest {
 
-    private final String companyName = "Kate%s".formatted(runId);
+    private static final String COMPANY_NAME = "Kate%s".formatted(runId);
 
     @BeforeClass
     @Override
     protected void beforeClass() {
         super.beforeClass();
-        TestUtils.createCompany(getApiRequestContext(), companyName);
+        TestUtils.createCompany(getApiRequestContext(), COMPANY_NAME);
     }
 
     @Test
@@ -33,10 +33,9 @@ public class EditCompanyDialogTest extends BaseTest {
     @Description("Edit company info and save")
     public void testEditCompanyInfoAndSave() {
         CompaniesAndBusinessUnitsPage companiesAndBusinessUnitsPage = new DashboardPage(getPage())
-//                .refreshDashboard()
                 .clickSystemAdministrationLink()
                 .getSystemMenu().clickCompaniesAndBusinessUnitsTab()
-                .getSelectCompany().selectCompany(companyName)
+                .getSelectCompany().selectCompany(COMPANY_NAME)
                 .clickEditCompanyButton()
                 .fillCompanyTypeField("LLC")
                 .fillCompanyDescriptionField("Description of company business model")
@@ -53,14 +52,14 @@ public class EditCompanyDialogTest extends BaseTest {
                 .clickSaveChangesButton();
 
         Allure.step("Verify: success message is displayed");
-        assertThat(companiesAndBusinessUnitsPage.getAlert().getMessage()).hasText(
-                "SUCCESSCompany was updated successfully");
+        assertThat(companiesAndBusinessUnitsPage.getAlert().getMessage())
+                .hasText("SUCCESSCompany was updated successfully");
     }
 
     @AfterClass
     @Override
     protected void afterClass() {
-        TestUtils.deleteCompany(getApiRequestContext(), companyName);
+        TestUtils.deleteCompany(getApiRequestContext(), COMPANY_NAME);
         super.afterClass();
     }
 }
