82be6a7de190ae1c8572e282c8c1a4f66ce378b6
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-05-28 09:12:28 +0300

    one more test run

--- src/test/java/xyz/npgw/test/run/ReportsPageTest.java
+++ src/test/java/xyz/npgw/test/run/ReportsPageTest.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.Constants;
 import xyz.npgw.test.common.base.BaseTest;
