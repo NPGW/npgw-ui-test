9c2b49e54bc06e2640dec0344dfa9636505fca00
Serhii Malko <malsv@ukr.net>
2025-05-30 11:45:06 +0300

    resolve

--- src/test/java/xyz/npgw/test/run/AddAcquirerDialogTest.java
+++ src/test/java/xyz/npgw/test/run/AddAcquirerDialogTest.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.common.entity.Acquirer;
@@ -20,9 +19,7 @@ import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.testng.Assert.assertEquals;
-import static xyz.npgw.test.common.util.TestUtils.createAcquirer;
-import static xyz.npgw.test.common.util.TestUtils.deleteAcquirer;
-import static xyz.npgw.test.common.util.TestUtils.getAcquirer;
+import static xyz.npgw.test.common.util.TestUtils.*;
 
 public class AddAcquirerDialogTest extends BaseTest {
 
