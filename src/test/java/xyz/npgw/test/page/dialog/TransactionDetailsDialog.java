c7a2e7761739799b1e2847000e27f8d9c2ef6dfd
Natasha <natashashimkiv@gmail.com>
2025-06-03 12:13:42 +0400

    Added test Check the hiding of parameters

--- src/test/java/xyz/npgw/test/page/dialog/TransactionDetailsDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/TransactionDetailsDialog.java
@@ -39,6 +39,7 @@ public class TransactionDetailsDialog extends BaseDialog<TransactionsPage, Trans
     @Step("Click on chevron in Card details section")
     public TransactionDetailsDialog clickChevronInCardDetailsSection() {
         chevron.click();
+        getPage().waitForTimeout(500);
         return new TransactionDetailsDialog(getPage());
     }
 }
