5905423e40c8f7eb41a126aedbdecadf8ee20876
AllaBird <allavorobieva@yahoo.com>
2025-06-07 18:59:19 -0400

    [ATC-10.010.00] Delete company

--- src/test/java/xyz/npgw/test/run/CompaniesAndBusinessUnitsTest.java
+++ src/test/java/xyz/npgw/test/run/CompaniesAndBusinessUnitsTest.java
@@ -5,14 +5,26 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
+import xyz.npgw.test.common.util.TestUtils;
 import xyz.npgw.test.page.DashboardPage;
 import xyz.npgw.test.page.system.CompaniesAndBusinessUnitsPage;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.testng.Assert.assertTrue;
 
 public class CompaniesAndBusinessUnitsTest extends BaseTest {
+
+    private static final String COMPANY_NAME = "%s company name".formatted(RUN_ID);
+
+    @BeforeClass
+    @Override
+    protected void beforeClass() {
+        super.beforeClass();
+        TestUtils.createCompany(getApiRequestContext(), COMPANY_NAME);
+    }
 
     @Test
     @TmsLink("691")
@@ -35,5 +47,27 @@ public class CompaniesAndBusinessUnitsTest extends BaseTest {
 
         Allure.step("Verify: company info block is visible again after selecting 'Show' in settings");
         assertThat(companiesAndBusinessUnitsPage.getCompanyInfoBlock()).isVisible();
+    }
+
+
+    @Test
+    @TmsLink("723")
+    @Epic("System/Companies and business units")
+    @Feature("Delete Company")
+    @Description("Delete company")
+    public void testDeleteCompany() {
+        CompaniesAndBusinessUnitsPage companiesAndBusinessUnitsPage = new DashboardPage(getPage())
+                .clickSystemAdministrationLink()
+                .getSystemMenu().clickCompaniesAndBusinessUnitsTab()
+                .getSelectCompany().selectCompany(COMPANY_NAME)
+                .clickDeleteSelectedCompany()
+                .clickDeleteButton();
+
+        Allure.step("Verify: the success alert appears after deleting the company");
+        assertThat(companiesAndBusinessUnitsPage.getAlert().getMessage())
+                .hasText("SUCCESSCompany was deleted successfully");
+
+        Allure.step("Verify: the deleted company is no longer present in the dropdown list");
+        assertTrue(companiesAndBusinessUnitsPage.getSelectCompany().isCompanyAbsentInDropdown(COMPANY_NAME));
     }
 }
