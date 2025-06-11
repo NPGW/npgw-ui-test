a158f3d8fe0a2d83fd41e120f2c4e5cf7248e2da
ElenaDetusheva <e.det@list.ru>
2025-06-11 09:42:37 +0300

    add testCloseButton

--- src/test/java/xyz/npgw/test/run/TransactionsPageTest.java
+++ src/test/java/xyz/npgw/test/run/TransactionsPageTest.java
@@ -645,7 +645,7 @@ public class TransactionsPageTest extends BaseTest {
     }
 
     @Test
-    @TmsLink("")
+    @TmsLink("736")
     @Epic("Transactions")
     @Feature("Reset filter")
     @Description("Verify, that ")
@@ -673,9 +673,23 @@ public class TransactionsPageTest extends BaseTest {
                 .clickResetFilterButton();
 
         Allure.step("Verify: the 'Data' input field value is current month after reset");
-        assertThat(transactionsPage.getSelectDateRange().getDateRangeField()).hasText(currentRange);
+        assertThat(transactionsPage.getDialog()).hasText(currentRange);
     }
 
+    @Test
+    @TmsLink("738")
+    @Epic("Transactions")
+    @Feature("Transaction details")
+    @Description("Check the Transaction detail dialog is closed after clicking on 'Close' button" )
+    public void testCloseButton() {
+        TransactionsPage transactionsPage= new DashboardPage(getPage())
+                .clickTransactionsLink()
+                .getTable().clickOnTransaction()
+                .clickCloseButton();
+
+        Allure.step("Verify: Transaction detail dialog is closed ");
+        assertThat(transactionsPage.getDialog()).not().isAttached();
+    }
     @AfterClass
     @Override
     protected void afterClass() {
