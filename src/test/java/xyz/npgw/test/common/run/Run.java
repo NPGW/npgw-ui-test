2e28d737084be79bb2c2a03494a8de376a880322
xtbtds <liza.xtbtds@gmail.com>
2025-06-12 15:21:16 +0200

    create fat jar and dockerize ui tests

--- src/test/java/xyz/npgw/test/common/run/Run.java
+++ src/test/java/xyz/npgw/test/common/run/Run.java
@@ -2,7 +2,6 @@ package xyz.npgw.test.common.run;
 
 import lombok.extern.slf4j.Slf4j;
 import org.testng.TestNG;
-import org.testng.xml.XmlClass;
 import org.testng.xml.XmlPackage;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
@@ -13,18 +12,13 @@ import java.util.List;
 public class Run {
 
     public static void main(String[] args) {
-        XmlSuite suite = new XmlSuite();
-        suite.setParallel(XmlSuite.ParallelMode.NONE);
-
-        for (XmlClass c : new XmlPackage("xyz.npgw.test.run.*").getXmlClasses()) {
-            System.out.println(c);
-            XmlTest t = new XmlTest(suite);
-            t.setName(c.getName());
-            t.setXmlClasses(List.of(c));
-        }
-
         TestNG testng = new TestNG();
-        testng.setXmlSuites(List.of(suite));
+
+        XmlSuite xmlSuite = new XmlSuite();
+        new XmlTest(xmlSuite).setXmlClasses(new XmlPackage("xyz.npgw.test.run.*").getXmlClasses());
+        testng.setXmlSuites(List.of(xmlSuite));
+
         testng.run();
+        System.exit(testng.getStatus());
     }
 }
