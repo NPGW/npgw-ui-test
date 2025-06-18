7047cdb24cb9b63074bca0f837f6186973f2293a
Natasha <natashashimkiv@gmail.com>
2025-06-18 13:42:12 +0400

    Refactoring test

--- src/test/java/xyz/npgw/test/page/dialog/TransactionDetailsDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/TransactionDetailsDialog.java
@@ -33,14 +33,18 @@ public class TransactionDetailsDialog extends BaseDialog<TransactionsPage, Trans
 
     @Step("Click on chevron in Card details section")
     public TransactionDetailsDialog clickChevronInSection(String sectionName) {
+
             Locator section = locator("div[aria-label='" + sectionName + "']");
+
             Locator chevron = section.locator("svg[role='presentation']");
+
             chevron.click();
         return new TransactionDetailsDialog(getPage());
     }
 
     private Locator parameter(String parameter, String sectionName) {
         Locator section = locator("div[aria-label='" + sectionName + "']");
+
         return section.locator("//div[.='" + parameter + "']/following-sibling::div[1]");
     }
 }
