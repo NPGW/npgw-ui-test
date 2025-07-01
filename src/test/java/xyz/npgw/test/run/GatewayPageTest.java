fec3bb2d80947aceedf625195fa363bea90f3d4c
PavelSZh <psilenkov@gmail.com>
2025-07-01 20:58:08 +0300

    Up and Down Buttons are joined

--- src/test/java/xyz/npgw/test/run/GatewayPageTest.java
+++ src/test/java/xyz/npgw/test/run/GatewayPageTest.java
@@ -241,7 +241,6 @@ public class GatewayPageTest extends BaseTest {
                 .clickAddMerchantAcquirer()
                 .getSelectAcquirer().selectAcquirer(ACQUIRER.acquirerName())
                 .clickCreateButton();
-//                .getAlert().waitUntilSuccessAlertIsGone();
 
         Allure.step("Verify the result of adding Acquirer within Gateway page table");
         assertThat(page.getMerchantFirstRowValue()).hasText(expectedBusinessUnitsList[2]);
@@ -253,11 +252,12 @@ public class GatewayPageTest extends BaseTest {
     }
 
     @Test
-    @TmsLink("835")
+    @TmsLink("835/847")
     @Epic("System/Gateway")
     @Feature("Merchant acquirer")
-    @Description("Move merchant-acquirer down to reduce their priority")
-    public void testMoveMerchantAcquirerDownButton() {
+    @Description("Move merchant-acquirer down to reduce their priority,"
+            + " Move merchant-acquirer up to increase their priority")
+    public void testMoveMerchantAcquirerDownAndUpButtons() {
         GatewayPage page = new DashboardPage(getPage())
                 .clickSystemAdministrationLink()
                 .getSystemMenu().clickGatewayTab()
@@ -279,35 +279,12 @@ public class GatewayPageTest extends BaseTest {
         Allure.step("Check that the second created acquirer priority is 0 now");
         assertThat(page.getAcquirerPriorityFirstRowValue()).hasText("0");
         assertThat(page.getAcquirerNameFirstRowValue()).hasText(ACQUIRER_MOVE.acquirerDisplayName());
-    }
-
-    @Test
-    @TmsLink("847")
-    @Epic("System/Gateway")
-    @Feature("Merchant acquirer")
-    @Description("Move merchant-acquirer up to increase their priority")
-    public void testMoveMerchantAcquirerUpButton() {
-        GatewayPage page = new DashboardPage(getPage())
-                .clickSystemAdministrationLink()
-                .getSystemMenu().clickGatewayTab()
-                .getSelectCompany().selectCompany(COMPANY_NAME)
-                .getSelectBusinessUnit().selectBusinessUnit(expectedBusinessUnitsList[2])
-                .clickAddMerchantAcquirer()
-                .getSelectAcquirer().selectAcquirer(ACQUIRER.acquirerName())
-                .clickCreateButton()
-                .clickAddMerchantAcquirer()
-                .getSelectAcquirer().selectAcquirer(ACQUIRER_MOVE.acquirerName())
-                .clickCreateButton();
-
-        Allure.step("Check that the first created acquirer priority is 0");
-        assertThat(page.getAcquirerPriorityFirstRowValue()).hasText("0");
-        assertThat(page.getAcquirerNameFirstRowValue()).hasText(ACQUIRER.acquirerDisplayName());
 
         page.clickMoveMerchantAcquirerUpButton(1);
 
-        Allure.step("Check that the second created acquirer priority is 0 now");
+        Allure.step("Check that the first created acquirer priority is 0 again");
         assertThat(page.getAcquirerPriorityFirstRowValue()).hasText("0");
-        assertThat(page.getAcquirerNameFirstRowValue()).hasText(ACQUIRER_MOVE.acquirerDisplayName());
+        assertThat(page.getAcquirerNameFirstRowValue()).hasText(ACQUIRER.acquirerDisplayName());
     }
 
     @Test
