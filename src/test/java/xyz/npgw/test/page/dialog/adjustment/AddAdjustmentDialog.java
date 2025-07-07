6e0d0e3b2d42933feec3e41f52d1aff3630a2267
PavelSZh <psilenkov@gmail.com>
2025-07-07 13:44:37 +0300

    A bit of test infrastructure for TransactionManagementPage https://github.com/NPGW/npgw-ui-test/issues/874 https://github.com/NPGW/npgw-ui-test/issues/873

---
+++ src/test/java/xyz/npgw/test/page/dialog/adjustment/AddAdjustmentDialog.java
@@ -0,0 +1,43 @@
+package xyz.npgw.test.page.dialog.adjustment;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
+import xyz.npgw.test.page.dialog.BaseDialog;
+import xyz.npgw.test.page.system.TransactionManagementPage;
+
+public class AddAdjustmentDialog extends BaseDialog<TransactionManagementPage, AddAdjustmentDialog> {
+
+    private final Locator selectTheFirstAvailableTransaction = locator("[aria-label='transactions table'] tr[data-first]");
+    private final Locator createButton = getByRole(AriaRole.BUTTON, "Create");
+    private final Locator closeButton = getByRole(AriaRole.BUTTON, "Close").first();
+
+    public AddAdjustmentDialog(Page page) {
+        super(page);
+    }
+
+    @Override
+    protected TransactionManagementPage getReturnPage() {
+        return new TransactionManagementPage(getPage());
+    }
+
+    public AddAdjustmentDialog clickOnTheFirstAvailableTransaction() {
+        selectTheFirstAvailableTransaction.click();
+
+        return this;
+    }
+
+    public TransactionManagementPage clickOnCreateButton() {
+        createButton.click();
+
+        return new TransactionManagementPage(getPage());
+    }
+
+    public TransactionManagementPage clickOnCloseButton() {
+        closeButton.click();
+        getDialog().waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.HIDDEN));
+
+        return new TransactionManagementPage(getPage());
+    }
+}
