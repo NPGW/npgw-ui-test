55354f15a3516666e2a7e3e357fd9153ec139592
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-07-08 09:42:03 +0300

    Ignore #873

--- src/test/java/xyz/npgw/test/common/logging/ApiLogger.java
+++ src/test/java/xyz/npgw/test/common/logging/ApiLogger.java
@@ -1,6 +1,7 @@
 package xyz.npgw.test.common.logging;
 
 import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.PlaywrightException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.SkipException;
@@ -23,7 +24,7 @@ public class ApiLogger {
                 break;
             case 5:
                 log.error("{} {} - {}", message, response.status(), response.text());
-                throw new SkipException(response.text());
+                throw new PlaywrightException(response.text());
             default:
                 log.debug("{} {} - {}", message, response.status(), response.text());
         }
