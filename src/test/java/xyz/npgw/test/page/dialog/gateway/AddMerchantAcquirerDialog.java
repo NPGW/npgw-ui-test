08c513911f864d76c67a4db5255328fe5eb8b8db
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-07-13 13:14:40 +0300

    Refactor clickCreateButton

--- src/test/java/xyz/npgw/test/page/dialog/gateway/AddMerchantAcquirerDialog.java
+++ src/test/java/xyz/npgw/test/page/dialog/gateway/AddMerchantAcquirerDialog.java
@@ -3,7 +3,6 @@ package xyz.npgw.test.page.dialog.gateway;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
-import com.microsoft.playwright.options.WaitForSelectorState;
 import lombok.Getter;
 import xyz.npgw.test.page.common.trait.AlertTrait;
 import xyz.npgw.test.page.common.trait.SelectAcquirerTrait;
@@ -29,8 +28,6 @@ public class AddMerchantAcquirerDialog extends BaseDialog<GatewayPage, AddMercha
 
     public GatewayPage clickCreateButton() {
         createButton.click();
-        getAlert().clickCloseButton();
-        getDialog().waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.HIDDEN));
 
         return new GatewayPage(getPage());
     }
