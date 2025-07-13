84d5449e6d22e4c1fd1c6973df0791534bbbd946
PavelSZh <psilenkov@gmail.com>
2025-07-13 14:58:22 +0300

    CheckStyle fixes

--- src/test/java/xyz/npgw/test/common/util/TestUtils.java
+++ src/test/java/xyz/npgw/test/common/util/TestUtils.java
@@ -4,10 +4,10 @@ import com.microsoft.playwright.APIRequestContext;
 import xyz.npgw.test.common.entity.BusinessUnit;
 import xyz.npgw.test.common.entity.Company;
 import xyz.npgw.test.common.entity.Info;
-import xyz.npgw.test.common.entity.User;
 import xyz.npgw.test.common.entity.MerchantAcquirer;
 import xyz.npgw.test.common.entity.Acquirer;
 import xyz.npgw.test.common.entity.FraudControl;
+import xyz.npgw.test.common.entity.User;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
