d19ec11f78b1a809f56a5ee3d708ca72c47f2b45
ElenaDetusheva <e.det@list.ru>
2025-07-25 19:17:19 +0300

    add testEditFraudControl

--- src/test/java/xyz/npgw/test/run/FraudControlTest.java
+++ src/test/java/xyz/npgw/test/run/FraudControlTest.java
@@ -247,6 +247,7 @@ public class FraudControlTest extends BaseTest {
         assertThat(cell).hasText("Inactive");
     }
 
+    @Ignore
     @Test(dependsOnMethods = {"testCancelAddingFraudControlToBusinessUnit",
             "testDeleteInactiveFraudControlAddedToBusinessUnit"})
     @TmsLink("910")
