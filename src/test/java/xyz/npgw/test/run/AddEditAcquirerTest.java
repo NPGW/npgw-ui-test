bb55f8fb76d4d4a325d3c669fcf133a1dc7a4e3c
PavelSZh <psilenkov@gmail.com>
2025-07-25 13:33:09 +0300

    testFilterByStatus - DataProvider fixed testSortByCreationDate - Ignored testTheVisibilityOfTheStatusSelectorOptions - fixed testResetStatus - fixed testPendingOccursAtMostOnceInLifecycle - Ignored testCheckTransactionDetails - fixed testAddCompanyFormValidationWhenEmpty - Ignored testAddCompany - temporary fixed testEditAcquirerVerifyUpdatedData - ignored

--- src/test/java/xyz/npgw/test/run/AddEditAcquirerTest.java
+++ src/test/java/xyz/npgw/test/run/AddEditAcquirerTest.java
@@ -8,6 +8,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.common.entity.Acquirer;
@@ -285,6 +286,7 @@ public class AddEditAcquirerTest extends BaseTest {
         assertEquals(actualPlaceholders, expectedPlaceholders);
     }
 
+    @Ignore("waiting for getByPlaceholder('Enter display name')")
     @Test
     @TmsLink("450")
     @Epic("System/Acquirers")
