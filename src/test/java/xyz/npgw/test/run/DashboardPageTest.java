47b8351f944e764d4d2a610b711781cdcefa004d
AllaBird <allavorobieva@yahoo.com>
2025-08-02 02:09:15 -0400

    merge

--- src/test/java/xyz/npgw/test/run/DashboardPageTest.java
+++ src/test/java/xyz/npgw/test/run/DashboardPageTest.java
@@ -9,6 +9,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.Constants;
 import xyz.npgw.test.common.base.BaseTest;
