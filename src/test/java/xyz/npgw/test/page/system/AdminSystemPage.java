2d55861f6c12f038ba13be2acc080bc78b3dfaf9
Serhii Malko <malsv@ukr.net>
2025-08-05 15:11:22 +0300

    Import statement

--- src/test/java/xyz/npgw/test/page/system/AdminSystemPage.java
+++ src/test/java/xyz/npgw/test/page/system/AdminSystemPage.java
@@ -5,7 +5,6 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.Step;
 import xyz.npgw.test.page.base.HeaderPage;
-import xyz.npgw.test.page.common.table.header.AdminHeaderMenuComponent;
 import xyz.npgw.test.page.common.table.header.AdminHeaderMenuTrait;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
--- src/test/java/xyz/npgw/test/run/AddEditAcquirerTest.java
+++ src/test/java/xyz/npgw/test/run/AddEditAcquirerTest.java
@@ -8,6 +8,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.common.entity.Acquirer;
@@ -124,6 +125,7 @@ public class AddEditAcquirerTest extends BaseTest {
         assertThat(statusRadiobutton).hasAttribute("data-selected", "true");
     }
 
+    @Ignore
     @Test
     @TmsLink("412")
     @Epic("System/Acquirers")
