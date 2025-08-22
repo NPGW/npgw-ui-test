902977150d79d7bac4913c0def4a04eae79c50f8
ElenaDetusheva <e.det@list.ru>
2025-08-22 13:19:44 +0300

    add testTooltips_TeamPage

--- src/test/java/xyz/npgw/test/page/system/SuperTeamPage.java
+++ src/test/java/xyz/npgw/test/page/system/SuperTeamPage.java
@@ -1,5 +1,9 @@
 package xyz.npgw.test.page.system;
 
+import io.qameta.allure.Step;
+import lombok.Getter;
+
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import lombok.extern.log4j.Log4j2;
 import xyz.npgw.test.page.component.header.SuperHeaderMenuTrait;
@@ -8,12 +12,16 @@ import xyz.npgw.test.page.component.system.SuperSystemMenuTrait;
 import xyz.npgw.test.page.component.table.SuperUsersTableTrait;
 import xyz.npgw.test.page.dialog.user.SuperAddUserDialog;
 
+@Getter
 @Log4j2
 public class SuperTeamPage extends BaseTeamPage<SuperTeamPage>
         implements SuperHeaderMenuTrait<SuperTeamPage>,
                    SuperSystemMenuTrait,
                    SelectCompanyTrait<SuperTeamPage>,
                    SuperUsersTableTrait {
+
+    private final Locator commonIconBtn = locator("[role='tabpanel']>div>div:first-child button[data-testid] svg");
+    private final Locator iconBtnModal = locator("[data-slot='content']");
 
     public SuperTeamPage(Page page) {
         super(page);
