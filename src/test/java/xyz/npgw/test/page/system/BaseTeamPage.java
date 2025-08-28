16363f5df0f667526df058d6f33e425cba91d8a4
Serhii Malko <malsv@ukr.net>
2025-08-28 21:13:37 +0300

    checkstyle

--- src/test/java/xyz/npgw/test/page/system/BaseTeamPage.java
+++ src/test/java/xyz/npgw/test/page/system/BaseTeamPage.java
@@ -1,10 +1,10 @@
 package xyz.npgw.test.page.system;
 
-import lombok.Getter;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.TimeoutError;
+import lombok.Getter;
 import io.qameta.allure.Step;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
@@ -12,6 +12,7 @@ import xyz.npgw.test.common.ProjectProperties;
 import xyz.npgw.test.common.entity.User;
 import xyz.npgw.test.page.base.HeaderPage;
 import xyz.npgw.test.page.component.select.SelectStatusTrait;
+
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
